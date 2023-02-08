3aae798f9bd1621caaffd85c36de2d9109099655
Ratiya Ranjit Bhurabhai <ranjitratiya7777@gmail.com>
2023-02-08 16:30:13 +0530

    undefined variables and modifying values of variables

--- main.js
+++ main.js
@@ -10,7 +10,20 @@
 
 
 // Variables in JavaScript
+/*
 var welcome_message = "This is a welcome message"
 var age = 21
 var exact_age = 21.4
 alert(welcome_message + ' ' +(age+exact_age))
+*/
+
+// Undefined variables and modifying values of variables
+var age = 21
+var myName = "Ranjit"
+console.log('My age is' + ' '+ age)
+console.log('My age next year will be '+ (age+1))
+age++
+console.log('My age next year will be '+ age++)
+age--
+console.log('My age last year will be '+ age--)
+console.log('My name is '+ myName)
