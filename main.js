1e27c638185c6ee95a3c795804c247b029d2e03f
Ratiya Ranjit Bhurabhai <ranjitratiya7777@gmail.com>
2023-02-08 16:19:24 +0530

    variables in js

--- main.js
+++ main.js
@@ -4,5 +4,13 @@
     Multi line comment
 */
 
-alert("This is Alert!!")
-console.log("Written in console")
+// alert("This is Alert!!")
+// console.log("Written in console")
+
+
+
+// Variables in JavaScript
+var welcome_message = "This is a welcome message"
+var age = 21
+var exact_age = 21.4
+alert(welcome_message + ' ' +(age+exact_age))
